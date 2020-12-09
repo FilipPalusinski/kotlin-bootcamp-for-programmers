c8cec28f1dc7000c61b1dcec5f11599b07314757
FilipPalusinski <palusinskifilip@gmail.com>
2020-12-09 23:35:01 +0100

    Update Lesson 4: OOP on last completed task: Use interface delegation.

--- Lesson 4 OOP/src/example/myapp/AquariumFish.kt
+++ Lesson 4 OOP/src/example/myapp/AquariumFish.kt
@@ -1,23 +1,37 @@
 package example.myapp
 
-abstract class AquariumFish: FishAction {
-    abstract val color: String
-    override fun eat() = println("yum")
-}
-
-class Shark: AquariumFish(), FishAction{
-    override val color = "gray"
-
-}
-class Plecostomus: AquariumFish(), FishAction{
-    override val color = "gold"
-    override fun eat(){
-        println("eat algae")
-    }
-}
 interface FishAction{
     fun eat()
 }
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat(){
+        println(food)
+    }
+}
+
+interface FishColor{
+    val color: String
+}
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+object GreyColor : FishColor {
+    override val color = "grey"
+}
+
+class Shark(fishColor: FishColor = GreyColor):
+        FishAction by PrintingFishAction("hunt and eat fish"),
+        FishColor by fishColor
+
+class Plecostomus(fishColor: FishColor = GoldColor):
+        FishAction by PrintingFishAction("eat algae"),
+        FishColor by fishColor
+
+
+
 
 
 
