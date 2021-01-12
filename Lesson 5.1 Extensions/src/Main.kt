69bd5608c137ba0ba6a9c8b3ab5634a9801404a3
FilipPalusinski <woodsonpl@gmail.com>
2021-01-12 19:04:07 +0100

    Update Lesson 5.1: Extensions, on last completed task: Comprehend collections.

--- Lesson 5.1 Extensions/src/Main.kt
+++ Lesson 5.1 Extensions/src/Main.kt
@@ -1,7 +1,53 @@
 
 
 fun main(){
-    runPairsAndTriples()
+    //runPairsAndTriples()
+    //runLists()
+    runHashMaps()
+}
+
+fun runHashMaps(){
+    //hashMaps that matches symptoms, the keys, with diseases of fish, the values.
+    val cures = hashMapOf("white spots" to "Ich", "red sores" to "hole disease")
+
+    //retrieve the disease value based on the symptom key
+    //using get
+    println(cures.get("white spots"))
+    //using square brackets
+    println(cures["red sores"])
+
+    //symptom that isn't in the map
+    println(cures["scale loss"])
+
+    //no match but set default value
+    println(cures.getOrDefault("bloating", "sorry, I don't know"))
+    //no match but do something else
+    println(cures.getOrElse("bloating"){"No cure for this"})
+
+    //mutable Map of elements, mean data can be changed
+    val inventory = mutableMapOf("fish net" to 1)
+    inventory.put("tank scrubber", 3)
+    println(inventory.toString())
+    inventory.remove("fish net")
+    println(inventory.toString())
+}
+
+fun runLists(){
+    //sum of integer elements
+    val list = listOf(1, 5, 3 ,4)
+    println(list.sum())
+
+    //sum of string elements
+    val list2 = listOf("a", "bbb", "cc")
+    println(list2.sumBy{ it.length })
+
+    //size of list
+    println(list2.size)
+
+    //list elements iterated and printed with space
+    for (s in list2.listIterator()) {
+        print("$s ")
+    }
 
 }
 
